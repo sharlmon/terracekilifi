5925be61108dd6be2b4430e41d1fa9a837fed3ec
sharlmon <sharlmon19@gmail.com>
2026-05-07 03:29:16 +0300

    deploy: setup github actions

--- src/pages/art-space.tsx
+++ src/pages/art-space.tsx
@@ -40,7 +40,7 @@ function ArtSpacePage() {
         eyebrow="The Art Space"
         title="Connecting, Inspiring, and Amplifying."
         description="A non-profit platform celebrating the power of art to gently shift perspectives. Rooted on Kilifi Creek, we draw on 20 years of collaboration to create vibrant cultural experiences."
-        image={IMAGES.ART_SPACE_HERO.src}
+        image={IMAGES.HERO_SUNSET.src}
         imageAlt="Art space gallery at The Terrace with creative exhibitions and performances"
       />
 
